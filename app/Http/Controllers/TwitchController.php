7d698743bd9f99a902893b1bf70e4a13610cf68e
josprimen <josebhpy@gmail.com>
2023-12-05 16:27:40 +0100

    * Primera prueba completa funcionando. He conseguido obtener los mejores clips de un canal de twitch y descargarlos automáticamente.

--- app/Http/Controllers/TwitchController.php
+++ app/Http/Controllers/TwitchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Carbon\Carbon;
 use Dflydev\DotAccessData\Data;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -41,36 +42,9 @@ class TwitchController extends Controller
             // URL de la página web que deseas capturar
             $url = 'https://www.twitch.tv/illojuan/clips?featured=false&filter=clips&range=30d';
 
-//            $url = 'https://www.google.com/';
-            $path = public_path('capturas/marca_com.png');
+            //Limpiamos el archivo
+            $this->guardarTextoEnArchivo('', 'twitch_web.txt', true);
 
-//            Browsershot::url($url)
-//                ->setNodeBinary('/usr/bin/node') // Ruta al ejecutable de Node.js
-//                ->setNpmBinary('/usr/bin/npm')   // Ruta al ejecutable de npm
-//                ->setChromePath('/usr/bin/chromium-browser') // Ruta al ejecutable de Chromium
-//                ->fullPage()
-//                ->save($path);
-
-//            Browsershot::url($url)
-//                ->setNodeBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/node') // Ruta específica de tu versión de Node.js
-//                ->setNpmBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/npm')   // Ruta específica de tu versión de npm
-//                ->setChromePath('/usr/bin/chromium-browser') // Ruta específica de tu instalación global de Chromium
-//                ->fullPage()
-//                ->save($path);
-
-
-//            Browsershot::url($url)
-//                ->setNodeBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/node') // Ruta específica de tu versión de Node.js
-//                ->setNpmBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/npm')   // Ruta específica de tu versión de npm
-//                ->setChromePath('/usr/bin/chromium-browser') // Ruta específica de tu instalación global de Chromium
-//                ->fullPage()
-//                ->noSandbox() // Agrega esta línea para deshabilitar el sandbox
-//                ->save($path);
-//
-//            dd('se ha guardado');
-//
-//
-//
 
             $body = Browsershot::url($url)
                 ->setNodeBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/node') // Ruta específica de tu versión de Node.js
@@ -79,7 +53,82 @@ class TwitchController extends Controller
                 ->setOption('waitUntil', 'networkidle0')
                 ->timeout(60000)
                 ->bodyHtml(); // returns the html of the body
-            dd($body);
+
+            $this->guardarTextoEnArchivo($body);
+
+//            dd($body);
+
+            // $crawler = new Crawler($html);
+            $text = Storage::get('twitch_web.txt');
+
+            $crawler = new Crawler($text);
+
+            // Selecciona todos los elementos que coinciden con el patrón
+            $elementos = $crawler->filter('a[data-a-target="preview-card-image-link"]');
+
+            // Inicializa un array para almacenar las URLs
+            $urls = [];
+
+            //Limpiamos el archivo.
+            $this->guardarTextoEnArchivo('', 'urls_clips.txt', true);
+
+            // Itera sobre cada elemento y extrae la URL del atributo href
+            $elementos->each(function (Crawler $elemento) use (&$urls) {
+                $url = $elemento->attr('href');
+
+                // Concatena el string "https://www.twitch.com" a la URL
+                $urlCompleta = "https://www.twitch.com" . $url;
+
+                // Muestra cada URL completa en la consola (puedes omitir esta línea si no es necesario)
+                //echo "URL completa: $urlCompleta\n";
+
+                // Agrega la URL completa al array
+                $urls[] = $urlCompleta;
+
+                //Añade la URL al archivo de URLs
+                $url_con_coma = $urlCompleta . ",\n";
+                $this->guardarTextoEnArchivo($url_con_coma, 'urls_clips.txt', false);
+
+
+            });
+
+
+            //Limpiamos el archivo.
+            $this->guardarTextoEnArchivo('', 'urls_videos.txt', true);
+
+
+            foreach ($urls as $url_video){
+                $body = Browsershot::url($url_video)
+                    ->setNodeBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/node') // Ruta específica de tu versión de Node.js
+                    ->setNpmBinary('/home/vagrant/.nvm/versions/node/v21.2.0/bin/npm')   // Ruta específica de tu versión de npm
+                    ->setChromePath('/usr/bin/chromium-browser') // Ruta específica de tu instalación global de Chromium
+                    ->setOption('waitUntil', 'networkidle0')
+                    ->timeout(60000)
+                    ->bodyHtml(); // returns the html of the body
+                $crawler = new Crawler($body);
+
+                // Selecciona todos los elementos que coinciden con el patrón de etiqueta video
+                $elementos = $crawler->filter('video');
+
+                // Inicializa un array para almacenar las URLs
+                $urls = [];
+
+                // Itera sobre cada elemento y extrae la URL del atributo src
+                $elementos->each(function (Crawler $elemento) use (&$urls) {
+                    $url = $elemento->attr('src');
+
+                    // Agrega la URL completa al array
+                    $urls[] = $url;
+                    $this->descargarVideo($url);
+
+                    // Añade la URL al archivo de URLs
+                    $url_con_coma = $url . ",\n";
+                    $this->guardarTextoEnArchivo($url_con_coma, 'urls_videos.txt', false);
+                });
+            }
+
+            dd('terminado proceso');
+
 
         }catch (\Exception $e){
             dd($e);
@@ -121,7 +170,7 @@ class TwitchController extends Controller
             }
 
             // Nombre del archivo de salida (puedes personalizarlo según tus necesidades)
-            $nombreArchivo = 'video_descargado.mp4';
+            $nombreArchivo = 'video_descargado' . Carbon::now()->timestamp . '.mp4';
 
             // Ruta completa del archivo de salida
             $rutaCompleta = $carpetaVideos . '/' . $nombreArchivo;
